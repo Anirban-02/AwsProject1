9a037084f076dfb18ecc3e3b02672d88de804db5
Anirban-02 <99980141+Anirban-02@users.noreply.github.com>
2023-04-05 16:15:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express()
 app.get('/', function (req, res) {
   var i,j;
   for(i=0;i<5;i++)
+  {
     for(j=0;j<i;j++)
       res.write('*');
     res.write('\n');
+  }
   res.end();
 })
 
