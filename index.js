0aa53a181ecf0c20c4b573b866d5689542ee49b0
Anirban-02 <99980141+Anirban-02@users.noreply.github.com>
2023-04-05 16:01:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello')
+  var i=0;
+  var j=0;
+  for (i=0;i<5;i++)
+    for(j=0;j<i;j++)
+      res.write('*');
+    res.write('\n');
+  res.end();
 })
 
 app.listen(4000, ()=>{
