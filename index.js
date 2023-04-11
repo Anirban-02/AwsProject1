ac7d3c9cb7e0eeccb09bfe01149a1e47cd62625e
Md Sohail <78078491+sohail3080@users.noreply.github.com>
2023-04-11 21:31:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  var i,j;
-  for(i=0;i<5;i++)
-  {
-    for(j=0;j<i;j++)
-      res.write('*');
-    res.write('\n');
-  }
-  res.end();
+  res.send('Hello')
 })
 
 app.listen(4000, ()=>{
